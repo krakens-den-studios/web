db9d3031a5711fd16ec8fd4c7f775dd7b1426387
CarlesRojas <carlesrojas@outlook.com>
2023-07-15 13:08:52 +0200

    dialog on mobile

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <div className="relative w-full h-1/2 lg:h-full">
           <Image
             src="/heartweaverCover.png"
-            className="object-cover lg:object-[24vw] 3xl:object-contain 3xl:object-[38vw] brightness-75"
+            className="object-cover lg:object-[24vw] 3xl:object-contain 3xl:object-[38vw] brightness-75 select-none"
             alt="Heart Weaver cover"
             fill
           />
@@ -17,7 +17,7 @@ export default function Home() {
 
         <div className="absolute bottom-0 left-0 right-0 lg:right-1/3 h-2/3 flex flex-col items-center gap-8">
           <div className="relative w-2/3 h-2/5">
-            <Image src="/heartweaver.svg" className="max-w-sm m-auto" fill alt="Heart Weaver title" />
+            <Image src="/heartweaver.svg" className="max-w-sm m-auto select-none" fill alt="Heart Weaver title " />
           </div>
 
           <div className="relative w-fit flex flex-col items-center gap-8 lg:flex-row">
