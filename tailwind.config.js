575ee4a353429d2586c6540c41993c67130c8666
CarlesRojas <carlesrojas@outlook.com>
2023-07-15 09:42:30 +0200

    add fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,12 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}'
   ],
   theme: {
-    extend: {}
+    extend: {
+      fontFamily: {
+        roboto: ['var(--roboto)'],
+        lora: ['var(--lora)']
+      }
+    }
   },
   plugins: []
 };
