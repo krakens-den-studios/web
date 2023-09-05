0c98e8c60d5f9acc11cdf8a3349d9a5f18cc813a
CarlesRojas <carlesrojas@outlook.com>
2023-09-05 11:01:11 +0200

    some fixes

--- app/games/heartweaver/page.tsx
+++ app/games/heartweaver/page.tsx
@@ -52,65 +52,68 @@ export default function HeartWeaver() {
         </div>
       </section>
 
-      <section
-        className="relative w-full h-fit gap-4 bg-purple grid md:grid-areas-left md:grid-cols-left items-center justify-items-center pt-8 md:pt-0"
-        id="game"
-      >
-        <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit">
-          <Title title="A Story Begins..." />
+      <section className="relative w-full h-fit bg-purple flex items-center justify-center" id="game">
+        <div className="relative w-full max-w-[100rem] h-fit gap-4 grid md:grid-areas-left md:grid-cols-left items-center justify-items-center pt-8 md:pt-0">
+          <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit">
+            <Title title="A Story Begins..." />
 
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            HeartWeaver is an emotional story-driven, action adventure with a dynamic ability system.
-          </p>
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              HeartWeaver is an emotional story-driven, action adventure with a dynamic ability system.
+            </p>
 
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            Loss and guilt have made Ace wish they could never feel anything again...
-          </p>
-        </div>
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              Loss and guilt have made Ace wish they could never feel anything again...
+            </p>
+          </div>
 
-        <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
-          <Image src="/heartweaverAce.png" className="object-contain object-bottom" fill alt="HeartWeaver Ace" />
-        </div>
-      </section>
-
-      <section className="relative w-full h-fit gap-4 grid md:grid-areas-right md:grid-cols-right items-center justify-items-center pt-8 md:pt-0">
-        <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit md:pl-8">
-          <Title title="Friends & Foes" />
-
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            Embark on a captivating journey through this twisted realm ensnared by the imbalance of Ace&apos;s emotions.
-          </p>
-
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            Help Ace embrace their emotions again and team up with Mark and their loved ones.
-          </p>
-        </div>
-
-        <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
-          <Image src="/heartweaverMark.png" className="object-contain object-bottom" fill alt="HeartWeaver Mark" />
+          <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
+            <Image src="/heartweaverAce.png" className="object-contain object-bottom" fill alt="HeartWeaver Ace" />
+          </div>
         </div>
       </section>
 
-      <section className="relative w-full h-fit gap-4 bg-purple grid md:grid-areas-left md:grid-cols-left items-center justify-items-center pt-8 md:pt-0">
-        <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit">
-          <Title title="Decide Ace's Fate" />
+      <section className="relative w-full h-fit flex items-center justify-center">
+        <div className="relative w-full h-fit gap-4 grid md:grid-areas-right md:grid-cols-right items-center justify-items-center pt-8 md:pt-0">
+          <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit md:pl-16">
+            <Title title="Friends & Foes" />
 
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            Hold the power to either restore Ace&apos;s emotions to their rightful harmony or let them fade forever...
-          </p>
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              Embark on a captivating journey through this twisted realm ensnared by the imbalance of Ace&apos;s
+              emotions.
+            </p>
 
-          <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
-            ... Ace&apos;s future rests in your hands.
-          </p>
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              Help Ace embrace their emotions again and team up with Mark and their loved ones.
+            </p>
+          </div>
+
+          <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
+            <Image src="/heartweaverMark.png" className="object-contain object-bottom" fill alt="HeartWeaver Mark" />
+          </div>
         </div>
+      </section>
 
-        <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
-          <Image
-            src="/heartweaverMark2.png"
-            className="object-contain object-center p-8 xl:p-16 2xl:p-24"
-            fill
-            alt="HeartWeaver book"
-          />
+      <section className="relative w-full h-fit bg-purple flex items-center justify-center" id="game">
+        <div className="relative w-full max-w-[100rem] h-fit gap-4 grid md:grid-areas-left md:grid-cols-left items-center justify-items-center pt-8 md:pt-0">
+          <div className="relative flex flex-col gap-6 items-center md:items-start md:grid-in-story w-fit">
+            <Title title="Decide Ace's Fate" />
+
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              Hold the power to either restore Ace&apos;s emotions to their rightful harmony or let them fade forever...
+            </p>
+
+            <p className="text-xl md:text-2xl w-11/12 max-w-3xl text-center md:text-left opacity-80">
+              ... Ace&apos;s future rests in your hands.
+            </p>
+          </div>
+          <div className="relative h-[40vh] lg:h-[50vh] xl:h-[60vh] w-full md:grid-in-image">
+            <Image
+              src="/heartweaverMark2.png"
+              className="object-contain object-center p-8 xl:p-16 2xl:p-24"
+              fill
+              alt="HeartWeaver book"
+            />
+          </div>
         </div>
       </section>
     </main>
